e11787eb4e00078ad7c42b4ef49e6dfc75ee42c0
StlMaris123 <stellamarisnjage@gmail.com>
2016-05-30 13:32:13 +0300

    full title helper test

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ root 'static_pages#home'
 get 'help' => 'static_pages#help'
 get 'about' => 'static_pages#about'
 get 'contact' => 'static_pages#contact'
+get 'resume' => 'static_pages#resume'
 get 'signup' => 'users#new'
 end
