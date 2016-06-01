001943ad3bdb9db849f7f22d77eb82ab7a6e31f5
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-01 12:53:49 +0300

    minimum password length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,5 +40,10 @@ test "email address should be unique" do
 	@user.save
 	assert_not duplicate_user.valid?
 end
+test "password should have a minimum length" do
+	@user.password = @user.password_confirmation = "a" * 5
+	assert_not @user.valid?
+end
+
 end
 
