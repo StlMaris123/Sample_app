f702a8c0091bbf1a6d7da9d0a025bbaa214e0c3a
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-03 11:49:20 +0300

    unsuccessful sign ups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+  def create
+  	@user = User.new(user_params)    #not the final implementation
+  	if @user.save
+  		#handle a successfl save
+  	else 
+  		render 'new'
+end
+end
+private
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :passwor_confirmation)
+  end
 end
