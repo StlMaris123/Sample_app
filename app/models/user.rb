859a5206ce15cf106b675a43958f09b2abacdfde
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-06 22:50:25 +0300

    adding remember and authenticated? methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,10 +8,10 @@ class User < ActiveRecord::Base
 						uniqueness: { case_sensitive: false }
 has_secure_password
 validates :password, length: { minimum: 6}
-<<<<<<< HEAD
+
 #Returns the hash digest of the given string
 def User.digest(string)
-	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
+	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 												  BCrypt::Engine.cost
     BCrypt::password.create(string, cost: cost)
 end
@@ -24,13 +25,26 @@ def remember
 	update_attribute(:remember_digest, User.digest(remember_token))
 end
 def authenticated?(remember_token)
-	BCrypt::Password.new(remeber_digest).is_password?(remember_token)
-=======
+	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end
 #returns the hash digest of the given string
 def User.digest(string)
 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 												  BCrypt::Engine.cost
 	BCrypt::Password.create(string,cost: cost)
->>>>>>> log-in-log-out
+
+end
+# Returns a random token.
+def User.new_token
+  SecureRandom.urlsafe_base64
+end
+# Remembers a user in the database for use in persistent sessions.
+def remember
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
+end
+# Returns true if the given token matches the digest.
+def authenticated?(remember_token)
+	BCrypt::Password.new(remember_digest).is_password?(remember_token)
 end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-<<<<<<< HEAD
 	test "layout links" do
 		get root_path
 		assert_template 'static_pages/home'
@@ -9,22 +8,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
-   
-  # test "the truth" do
-  #   assert true
   end
-=======
+  end
 
-test "layout links" do
-get root_path
-assert_template 'static_pages/home'
-assert_select "a[href=?]", root_path, count: 2
-assert_select "a[href=?]", help_path
-assert_select "a[href=?]", about_path
-assert_select "a[href=?]", contact_path
-get signup_path
-
-
-end
->>>>>>> log-in-log-out
-end
