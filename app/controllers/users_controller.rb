8938cf6283bf991545ff06f685a80a627805fe7b
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-07 10:45:41 +0300

    Creating an edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find_by(id: params[:id])
-		
-	end
+		end
+
   def new
   	@user = User.new
   end
+
   def create
   	@user = User.new(user_params)    #not the final implementation
   	if @user.save
@@ -16,6 +17,11 @@ class UsersController < ApplicationController
   		render 'new'
 end
 end
+
+def edit
+  @user = User.find(params[:id])
+end
+
 private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
