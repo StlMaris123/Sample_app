51dd9a517f3977c65d8600fa950050aa07ab13e6
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-09 10:43:42 +0300

    destroying microposts if user is destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,23 +34,34 @@ invalid_addresses.each do |invalid_address|
 assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
 end
 end
+
 test "email address should be unique" do
 	duplicate_user = @user.dup
 	duplicate_user.email = @user.email.upcase
 	@user.save
 	assert_not duplicate_user.valid?
 end
+
 test "password should have a minimum length" do
 	@user.password = @user.password_confirmation = "a" * 5
 	assert_not @user.valid?
 end
+
 test "email should be saved as lower-case" do
 	mixed_case_email = "Foo@ExAMPle.CoM"
 	@user.email = mixed_case_email
 	@user.save
 	assert_equal mixed_case_email.downcase, @user.reload.email
 end
+
 test "authenticated? should return false for a user with nil digest" do
 	assert_not @user.authenticated?('')
+end
+test "associated micropost should be destroyed" do
+	@user.save
+	@user.microposts.create!(content: "wawawa")
+	assert_difference 'Micropost.count', -1 do
+		@user.destroy
+	end
 end
 end
