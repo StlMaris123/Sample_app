806e7d8d941474eb8a7e874c5b32c1ab776f3519
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-10 09:49:13 +0300

    adding authorisation to micropost controller action

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
 
 resources :users
+resources :account_activations, only: [:edit]
+resources :password_resets,     only: [:new, :create, :edit, :update]
+resources :microposts,          only: [:create, :destroy]
 end
