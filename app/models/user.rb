89425011fec6f743a0457cf4f3bcaff93db02d8c
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-10 12:10:48 +0300

    adding a proto-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,41 +16,52 @@ def User.digest(string)
 												  BCrypt::Engine.cost
     BCrypt::password.create(string, cost: cost)
 end
+
 # returns a random token
 def User.new_token
 	SecureRandom.urlsafe_base64
 end
+
 # remebers a user in the database for use in persistent sessions
 def remember
 	self.remember_token = User.new_token
 	update_attribute(:remember_digest, User.digest(remember_token))
 end
+
 def authenticated?(remember_token)
 	BCrypt::Password.new(remember_digest).is_password?(remember_token)
 end
+
 #returns the hash digest of the given string
 def User.digest(string)
 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 												  BCrypt::Engine.cost
 	BCrypt::Password.create(string,cost: cost)
-
 end
+
 # Returns a random token.
 def User.new_token
   SecureRandom.urlsafe_base64
 end
+
 # Remembers a user in the database for use in persistent sessions.
 def remember
   self.remember_token = User.new_token
   update_attribute(:remember_digest, User.digest(remember_token))
 end
+
 # Returns true if the given token matches the digest.
 def authenticated?(remember_token)
 	return false if remember_digest.nil?
 	BCrypt::Password.new(remember_digest).is_password?(remember_token)
 end
+
 def forget
 	update_attribute(:remember_digest, nil)
 
+end
+
+def feed
+	Micropost.where("user_id = ? ", id)
 end
 end
