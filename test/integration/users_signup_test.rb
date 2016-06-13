1d517e2e941f3546ce411d23c6529c04236b274e
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-13 12:54:25 +0300

    adding account activation to user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,7 +19,7 @@ post_via_redirect users_path, user: { name: "Example User",
 									password:"password",
 									password_confirmation: "password" }
 end
-assert_template 'users/show'
-assert is_logged_in?
+#assert_template 'users/show'
+#assert is_logged_in?
 end
 end
