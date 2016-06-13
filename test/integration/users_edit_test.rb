a4c6facdd86041c318674e02444b19f8fd5d26c7
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-13 16:52:56 +0300

    chapter 9 exercise

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -26,7 +26,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
  									password_confirmation: "" }
  	assert_not flash.empty?
  	assert_redirected_to @user
- 	assert session[forwarding_url].nil?
+ 	assert_nil session[:forwarding_url]
+ 	#assert_equal session[:forwarding_url], nil
  	@user.reload
  	assert_equal @user.name, name
  	assert_equal @user.email, email
