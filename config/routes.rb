9119ebed90792bc48e45c040f03813fde6c41f96
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-14 10:10:35 +0300

    password reset view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
  
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
 root 'static_pages#home'
 get 'help' => 'static_pages#help'
 get 'about' => 'static_pages#about'
@@ -12,7 +16,7 @@ post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
 
 resources :users
-resources :account_activations, only: [:edit]
-#resources :password_resets,     only: [:new, :create, :edit, :update]
-resources :microposts,          only: [:create, :destroy]
+resources :account_activations, only:   [:edit]
+resources :password_resets,     except: [:index, :destroy]
+resources :microposts,          only:   [:create, :destroy]
 end
