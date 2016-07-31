5944d6f57f24cefbe165c0a3f721570c0f697c76
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-31 22:23:40 +0300

    style the controllers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,15 +3,19 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
-end
 
   def help
   end
+
   def about
   end
+
   def contact
   end
+
   def resume
   end
+
 end
